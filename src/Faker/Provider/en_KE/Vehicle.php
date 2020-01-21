5b8bab0ecaadee4b3a60167cc817f0082cb86602
Samuel Wanjiku <mwangithegreat@gmail.com>
2020-01-21 15:02:37 +0300

    Added support for ->vehicleRegistration() to create Fake vehicle registrations with Kenyan Licence plates

---
+++ src/Faker/Provider/en_KE/Vehicle.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Faker\Provider\en_KE;
+
+
+use Faker\Generator;
+use Illuminate\Support\Str;
+
+class Vehicle extends \Faker\Provider\Base
+{
+    protected $generator;
+
+    protected static $vehicleRegistration = array('KA? ###?','KB? ###?','KC? ###?');
+
+    protected static $vin = array('KA? ###?','KB? ###?','KC? ###?');
+
+    public function __construct(Generator $generator)
+    {
+        parent::__construct($generator);
+    }
+
+    /**
+     * Get vehicle registration number
+     *
+     * @param string $regex
+     *
+     * @return string
+     */
+    public function vehicleRegistration() : string
+    {
+        return Str::upper(static::bothify($this->generator->parse(static::randomElement(static::$vehicleRegistration))));
+    }
+}
