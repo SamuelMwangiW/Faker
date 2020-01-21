573ea714b2deccf1b64f4634850ff2f48dd94909
Sam <samuel@samuelmwangi.co.ke>
2020-01-21 22:57:55 +0300

    Added a misc class. To fork to a seperate repo at a later date

---
+++ src/Faker/Provider/en_KE/Miscellaneous.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Faker\Provider\en_KE;
+
+
+use Faker\Generator;
+use Illuminate\Support\Str;
+
+class Miscellaneous extends \Faker\Provider\Base
+{
+    protected static $policyNumber = array(
+        'POL/FKR/MPTK/#######/###/20##',
+        'MP/######',
+        'P/???/####/20##/######/',
+        'P-20##-1##-9###-##-#####',
+        '0##?#########');
+
+    public function __construct(Generator $generator)
+    {
+        parent::__construct($generator);
+    }
+
+    /**
+     * @return string
+     */
+    public function policyNumber(): string
+    {
+        return Str::upper(
+            static::bothify($this->generator->parse(static::randomElement(static::$policyNumber)))
+        );
+    }
+}
