514ec4f84068633cfd6bff74c2367d98b9d8f537
Samuel Wanjiku <samuel@samuelmwangi.co.ke>
2020-01-22 09:54:11 +0300

    Added ->vin() to Vehicle

--- src/Faker/Provider/en_KE/Vehicle.php
+++ src/Faker/Provider/en_KE/Vehicle.php
@@ -13,8 +13,6 @@ class Vehicle extends \Faker\Provider\Base
 
     protected static $vehicleRegistration = array('KA? ###?','KB? ###?','KC? ###?');
 
-    protected static $vin = array('KA? ###?','KB? ###?','KC? ###?');
-
     public function __construct(Generator $generator)
     {
         parent::__construct($generator);
@@ -32,5 +30,10 @@ class Vehicle extends \Faker\Provider\Base
         return Str::upper(
 			static::bothify($this->generator->parse(static::randomElement(static::$vehicleRegistration)))
 		);
+    }
+
+    public function vin() : string
+    {
+        return $this->generator->regexify('([A-HJ-NPR-Z0-9]{8})([A-Z0-9]{2})([A-HJ-NPR-Z0-9]{7})');
     }
 }
